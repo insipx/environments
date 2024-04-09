dff568c7a1c565526a510025414028e822293ff9
Andrew Plaza <github@andrewplaza.dev>
2024-04-09 16:01:07 -0400

    pull rust-toolchain from libxmtp repo

--- envs/libxmtp.nix
+++ envs/libxmtp.nix
@@ -12,24 +12,23 @@ let
     owner = "xmtp";
     repo = "libxmtp";
     rev = "main";
-    hash = "sha256-0000000000000000000000000000000000000000000=";
+    hash = "sha256-CyvPg1mFA0wWMVgM+bOqzIa3mY8eKSR46rNlF/1h408=";
   };
 
-  # rust-toolchain = fenixPkgs.fromToolchainFile {
-  #   file = "${src}/rust-toolchain";
-  #   sha256 = "sha256-0000000000000000000000000000000000000000000=";
-  # };
+  rust-toolchain = fenixPkgs.fromToolchainFile {
+    file = "${src}/rust-toolchain";
+    sha256 = "sha256-3St/9/UKo/6lz2Kfq2VmlzHyufduALpiIKaaKX4Pq0g=";
+  };
 
-  # fenixPkgs.fromToolchainFile { file = "${src}/rust-toolchain"; };
-  rust-toolchain = with fenixPkgs;
-    combine [
-      minimal.rustc
-      minimal.cargo
-      complete.clippy
-      complete.rustfmt
-      complete.llvm-tools-preview
-      targets.wasm32-unknown-unknown.latest.rust-std
-    ];
+  #rust-toolchain = with fenixPkgs;
+  #  combine [
+  #    minimal.rustc
+  #    minimal.cargo
+  #    complete.clippy
+  #    complete.rustfmt
+  #    complete.llvm-tools-preview
+  #    targets.wasm32-unknown-unknown.latest.rust-std
+  #  ];
 in pkgs.mkShell {
   nativeBuildInputs = with pkgs; [ pkg-config ];
   buildInputs = with pkgs;
