b1941efdbf08e6b792ba088da9a16e4265a77ce2
Andrew Plaza <github@andrewplaza.dev>
2025-11-07 20:31:55 +0000

    add lnav package

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
     }:
     {
       overlays.default = final: prev: {
-        inherit (self.packages.${final.system}) jj-stack jj-gt;
+        inherit (self.packages.${final.system}) jj-stack jj-gt lnav;
       };
     } //
     flake-utils.lib.eachDefaultSystem (
@@ -76,6 +76,7 @@
         packages = {
           jj-stack = pkgs.callPackage ./pkgs/jj-stack { };
           jj-gt = pkgs.callPackage ./pkgs/jj-gt { };
+          lnav = pkgs.callPackage ./pkgs/lnav { };
         };
       }
     );
